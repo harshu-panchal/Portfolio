d999140eabd6c1d2350120b84cd95432657fca02
harshu-panchal <harshvardhanpanc145@gmail.com>
2024-10-12 20:15:37 +0530

    first commit

---
+++ src/components/Resume.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Resume.css'
+import Workexperience from './Workexperience'
+import Skills from './Skills'
+import Education from './Education'
+import pdf from "/HARSHVARDHAN_PANCHAL_resume.pdf"
+
+const Resume = () => {
+  return (
+    <><div className="resume-container">
+      <div className="resume-heading">
+        <span className='word'>Resume</span> 
+        <div className='low-width'>
+        <a href={pdf} download={true}><p className="download-btn">Download Resume</p></a></div>
+      </div>
+      <Workexperience />
+      <Skills />
+      <Education/>
+      </div>
+    </>
+  )
+}
+
+export default Resume
